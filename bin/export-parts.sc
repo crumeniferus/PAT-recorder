92bfb577bfef533ce2eb00ffa2e3841789cb8c1b
crumeniferus <ian.cheesbrough@gmail.com>
2015-06-20 15:14:03 +0100

    Added potential scripts for exporting report sections.

---
+++ bin/export-parts.sc
@@ -0,0 +1,3 @@
+echo tbl "PAT-title.tbl" title
+#echo tbl "PAT-header.tbl" header
+#echo tbl "PAT-data.tbl" main_data
---
+++ bin/export-parts.sh
@@ -0,0 +1,3 @@
+echo tbl \"PAT-title.tbl\" title > /dev/tty
+#echo tbl "PAT-header.tbl" header
+#echo tbl "PAT-data.tbl" main_data
